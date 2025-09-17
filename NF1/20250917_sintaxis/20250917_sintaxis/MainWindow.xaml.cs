1405bd76d76b7b22b8ec0078dfb680a76c8f359e
developrepogithub <develop.repo.github@gmail.com>
2025-09-17 17:27:11 +0200

    primer commit

--- NF1/20250917_sintaxis/20250917_sintaxis/MainWindow.xaml.cs
+++ NF1/20250917_sintaxis/20250917_sintaxis/MainWindow.xaml.cs
@@ -22,7 +22,59 @@ namespace _20250917_sintaxis
     {
         public MainWindow()
         {
-            InitializeComponent();
+           InitializeComponent();
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            String cadena = "Hola\n";
+            txtSortida.Text = cadena;
+
+            txtSortida.AppendText("Adeu !");
+
+            // Tipus primitius de dades
+            // 
+            float preu = 0.0f;
+            double preuDouble = 0.0;
+            // number(10,2)
+            decimal preus = 100.23m;
+            char c = 'A';
+            bool haArribat = false;
+
+            String nom = "Paco";
+            nom = nom.ToUpper();
+            mostra(nom);
+            nom = "Maria Pérez Montes";
+            string[] trocets = nom.Split(' ');
+            mostra(trocets[0]);
+            mostra(trocets[1]);
+            mostra(trocets[2]);
+
+
+            string cadenaBuida = "";
+            if (cadenaBuida.Equals(""))
+            {
+                mostra("La cadena està buida");
+            }
+            // ==================================
+            // Treballant amb dates
+            DateTime dateTime = DateTime.Now;
+            DateTime avui = DateTime.Today;
+            if (avui < dateTime)
+            {
+                mostra("BINGO!");
+            }
+            DateTime dataPersonalitzada = new DateTime(2010, 10, 10, 10, 10, 10, DateTimeKind.Utc);
+            mostra(dataPersonalitzada.ToString("dd/MM/yy"));
+            mostra(dataPersonalitzada.ToString("dddd, dd  MMMM \\de yyyy"));
+
+        }
+
+        private void mostra(string nom)
+        {
+            txtSortida.AppendText(nom + "\n");
         }
     }
 }
