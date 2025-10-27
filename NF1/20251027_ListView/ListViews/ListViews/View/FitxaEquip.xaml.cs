57a0a39b3190be0b533e719478e757c93dc6b688
developrepogithub <develop.repo.github@gmail.com>
2025-10-27 16:59:29 +0100

    ListView and User controls

---
+++ NF1/20251027_ListView/ListViews/ListViews/View/FitxaEquip.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListViews.View
+{
+    /// <summary>
+    /// Lógica de interacción para FitxaEquip.xaml
+    /// </summary>
+    public partial class FitxaEquip : UserControl
+    {
+        public FitxaEquip()
+        {
+            InitializeComponent();
+        }
+    }
+}
