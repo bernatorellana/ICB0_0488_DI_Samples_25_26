a91bfb4383b9d493c92e7818163659f4c350bf3d
developrepogithub <develop.repo.github@gmail.com>
2025-11-27 17:59:05 +0100

    Carta

---
+++ NF1/202511_27_CustomControls_Cartes/Cartes/Cartes/Model/Pal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cartes.Model
+{
+    internal enum Pal
+    {
+        COR,
+        DIAMANT,
+        PICA,
+        TREBOL,
+        JOKER
+    }
+}
---
+++ NF1/202511_27_CustomControls_Cartes/Cartes/Cartes/View/UCCarta.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Windows.Controls;
+using Cartes.Model;
+using DependencyPropertyGenerator;
+
+namespace Cartes.View
+{
+
+
+    [DependencyProperty<Pal>("DasPal", DefaultValue  = Pal.COR )]
+    [DependencyProperty<Rang>("DerRang", DefaultValue = Rang.A)]
+    [DependencyProperty<bool>("Visible", DefaultValue = true)]
+
+    /// <summary>
+    /// Lógica de interacción para UCCarta.xaml
+    /// </summary>
+    public partial class UCCarta : UserControl
+    {
+        public UCCarta()
+        {
+            InitializeComponent();
+        }
+    }
+}
