83d4df1ba6415ba3895bbbc1517063c38a37f03f
developrepogithub <develop.repo.github@gmail.com>
2025-12-11 18:05:12 +0100

    xx

--- NF1/20151211_Reports_PDFQuest/PDFQuest/PDFQuest/MainWindow.xaml.cs
+++ NF1/20151211_Reports_PDFQuest/PDFQuest/PDFQuest/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -7,7 +8,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
 
 namespace PDFQuest
 {
@@ -19,6 +23,51 @@ namespace PDFQuest
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnPDF_Click(object sender, RoutedEventArgs e)
+        {
+            QuestPDF.Settings.License = LicenseType.Community;
+            // code in your main method
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(QuestPDF.Helpers.Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20));
+
+                    page.Header()
+                        .Text("Hello PDF!")
+                        .SemiBold().FontSize(36).FontColor(QuestPDF.Helpers.Colors.Blue.Medium);
+
+                    page.Content()
+                        .PaddingVertical(1, Unit.Centimetre)
+                        .Column(x =>
+                        {
+                            x.Spacing(20);
+
+                            x.Item().Text(Placeholders.LoremIpsum());
+                            x.Item().Image(Placeholders.Image(200, 100));
+                        });
+
+                    page.Footer()
+                        .AlignCenter()
+                        .Text(x =>
+                        {
+                            x.Span("Page ");
+                            x.CurrentPageNumber();
+                        });
+                });
+            })//.GeneratePdfAndShow();
+            .GeneratePdf("hello.pdf");
+            String path = Path.GetFullPath("hello.pdf");
+
+            BrowserView.Source= new Uri("file:///"+path);
+
+
+
         }
     }
 }
