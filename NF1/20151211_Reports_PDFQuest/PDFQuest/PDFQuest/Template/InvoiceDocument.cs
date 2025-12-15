7c382c37066570f92b97838c942e8ba5e41dca63
developrepogithub <develop.repo.github@gmail.com>
2025-12-15 16:17:03 +0100

    ddd

--- NF1/20151211_Reports_PDFQuest/PDFQuest/PDFQuest/Template/InvoiceDocument.cs
+++ NF1/20151211_Reports_PDFQuest/PDFQuest/PDFQuest/Template/InvoiceDocument.cs
@@ -31,10 +31,41 @@ namespace PDFQuest.Template
                 {
                     page.Margin(50);
 
-                    page.Header().Height(100).Background(Colors.Grey.Lighten1);
+                    page.Header().Element(ComposeHeader);
                     page.Content().Background(Colors.Grey.Lighten3);
                     page.Footer().Height(50).Background(Colors.Grey.Lighten1);
                 });
         }
+
+        void ComposeHeader(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row.RelativeItem(100).Height(50).Image("Resources\\cosa.jpg");
+
+                row.RelativeItem(100).Column(column =>
+                {
+                    
+                    column.Item()
+                        .Text($"Invoice #{Model.InvoiceNumber}")
+                        .FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Issue date: ").SemiBold();
+                        text.Span($"{Model.IssueDate:d}");
+                    });
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Due date: ").SemiBold();
+                        text.Span($"{Model.DueDate:d}");
+                    });
+                });
+
+                row.RelativeItem(100).Height(50).Placeholder();
+            });
+        }
+
     }
 }
