c4231dba9df2076256716a34756683758a6f1ffe
DESKTOP-I7LNFTQ\Bernat <bernat.orellana@gmail.com>
2026-01-14 16:14:15 +0100

    Testos unitaris del teatre

--- NF1/20260112_Testing_Teatre/WpfTestingTeatre/WpfTestingTeatre/Model/TarifesTeatre.cs
+++ NF1/20260112_Testing_Teatre/WpfTestingTeatre/WpfTestingTeatre/Model/TarifesTeatre.cs
@@ -13,6 +13,25 @@ namespace WpfTestingTeatre.Model
             if (tipus_seient <= 0 || tipus_seient > 4) throw new Exception("Tipus de seient erroni");
             if (edat <= 0 || edat >= 120) throw new Exception("Edat erronia");
 
+
+            /*
+                 1 (platea) : 60€
+                 2 (laterals) : 50€
+                 3 (primer pis): 40€
+                 4 (segon pis): 30€ 
+             * */
+            decimal[] preus = { 0, 60, 50, 40, 30 };
+            if (edat < 4) return 0;
+            if (edat < 12) return preus[tipus_seient] * 0.75m;
+            if (edat < 60) return preus[tipus_seient];
+            else
+            {
+                if (tipus_seient==1) return preus[tipus_seient]*0.5m;
+                else { 
+                    return preus[tipus_seient]*0.2m;
+                }
+            }
+
             decimal result = 0;
             return result;
         }
