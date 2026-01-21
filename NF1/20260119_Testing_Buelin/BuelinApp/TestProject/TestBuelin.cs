c562792fcdde0e10d3730801abd384dd7da4e149
DESKTOP-I7LNFTQ\Bernat <bernat.orellana@gmail.com>
2026-01-21 16:23:27 +0100

    ddd

--- NF1/20260119_Testing_Buelin/BuelinApp/TestProject/TestBuelin.cs
+++ NF1/20260119_Testing_Buelin/BuelinApp/TestProject/TestBuelin.cs
@@ -5,7 +5,23 @@ namespace TestProject
     [TestClass]
     public sealed class TestBuelin
     {
+
+
         [TestMethod]
+        public void TestBuelinGetPreu_Erronis()
+        {
+            Buelin b = new Buelin();
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10], 15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10, 10], 15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, -10, 10], 15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 0], 15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10], 15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10], 15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null, 10, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null, null, [10, -10], false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null, null, [10, 0], false); });
+        }
+         [TestMethod]
         public void TestBuelinGetPreu()
         {
             Buelin b;
@@ -13,11 +29,11 @@ namespace TestProject
             
 
             Assert.AreEqual(0m, b.getSobrecostPerEquipatge(null, null, null, false));
-            Assert.AreEqual(0m, b.getSobrecostPerEquipatge([30, 30, 30,], null, null, false));
-            Assert.AreEqual(60m, b.getSobrecostPerEquipatge([31, 30, 30,], null, null, false));
-            Assert.AreEqual(60m, b.getSobrecostPerEquipatge([30, 31, 30,], null, null, false));
-            Assert.AreEqual(60m, b.getSobrecostPerEquipatge([30, 30, 31,], null, null, false));
-            Assert.AreEqual(20m, b.getSobrecostPerEquipatge([30, 30, 30,], null, null, false));
+            Assert.AreEqual(0m, b.getSobrecostPerEquipatge([30, 30, 30], null, null, false));
+            Assert.AreEqual(60m, b.getSobrecostPerEquipatge([31, 30, 30], null, null, false));
+            Assert.AreEqual(60m, b.getSobrecostPerEquipatge([30, 31, 30], null, null, false));
+            Assert.AreEqual(60m, b.getSobrecostPerEquipatge([30, 30, 31], null, null, false));
+            Assert.AreEqual(20m, b.getSobrecostPerEquipatge([30, 30, 30], null, null, false));
             Assert.AreEqual(0m, b.getSobrecostPerEquipatge(null, 20, [20, 20], false));
             Assert.AreEqual(20m, b.getSobrecostPerEquipatge(null, 20, [20, 21], false));
             Assert.AreEqual(200m, b.getSobrecostPerEquipatge(null, 20, [20, 20, 20], false));
