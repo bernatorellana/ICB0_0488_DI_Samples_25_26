57e698deed5cb91152aa9942ded7a4e8ef26f372
DESKTOP-I7LNFTQ\Bernat <bernat.orellana@gmail.com>
2026-01-21 17:20:15 +0100

    xxx

--- NF1/20260119_Testing_Buelin/BuelinApp/BuelinApp/Model/Buelin.cs
+++ NF1/20260119_Testing_Buelin/BuelinApp/BuelinApp/Model/Buelin.cs
@@ -15,8 +15,73 @@ namespace BuelinApp.Model
             int[] pesMaletes,
             bool pagaAmbTarja)
         {
+            if (midaEquipatgeMaCm == null && pesEquipatgeMa != null)
+            {
+                error("Equipatge de mà: mida nula i pes no nul!!");
+            }
+            if (midaEquipatgeMaCm != null)
+            {
+                if (midaEquipatgeMaCm.Length != 3)
+                    error("Equipatge de mà: mida ha de tenir 3 components (x,y,z)");
+                else
+                    foreach (int p in midaEquipatgeMaCm)
+                    {
+                        if (p <= 0) error("Equipatge de mà: dimensions errònies");
+                    }
+            }
+            if (pesEquipatgeMa<=0) error("Equipatge de mà: pes erroni");
             
-            return 0;
+            
+            if (pesMaletes != null)
+            {
+                foreach (int p in pesMaletes)
+                {
+                    if (p <= 0) error("Malete: dimensions errònies");
+                }
+            }
+            //===================================
+            decimal sobrecostEquipatgeDeMa = 0m;
+            if (midaEquipatgeMaCm != null)
+            {
+                if( midaEquipatgeMaCm.Count(m=>m.Value>30) >0){
+                    sobrecostEquipatgeDeMa = 60m;
+                } else if (pesEquipatgeMa.HasValue && pesEquipatgeMa.Value > 20)
+                {
+                    sobrecostEquipatgeDeMa = (pesEquipatgeMa.Value - 20m)*20m;
+                }
+            }
+
+
+            //===================================
+            decimal sobrecostMaletes = 0m;
+            if(pesMaletes!= null)
+            for (int i = 0; i < pesMaletes.Length; i++)
+            {
+                    if (i < 2)
+                    {
+                        sobrecostMaletes += (pesMaletes[i] - 20) * 20;
+                    }
+                    else
+                    {
+                        sobrecostMaletes += pesMaletes[i] * 10;
+                    }
+                    }
+            
+           
+
+            //===================================
+            decimal sobrecostTarjaDeCredit = 0m;
+            if (sobrecostEquipatgeDeMa + sobrecostMaletes > 0)
+            {
+                sobrecostTarjaDeCredit += pagaAmbTarja ? 2 : 0;
+            }
+
+            return sobrecostEquipatgeDeMa+sobrecostMaletes + sobrecostTarjaDeCredit;
+        }
+
+        private static void error(string msg)
+        {
+            throw new ArgumentException(msg);
         }
     }
 }
--- NF1/20260119_Testing_Buelin/BuelinApp/TestProject/TestBuelin.cs
+++ NF1/20260119_Testing_Buelin/BuelinApp/TestProject/TestBuelin.cs
@@ -11,22 +11,22 @@ namespace TestProject
         public void TestBuelinGetPreu_Erronis()
         {
             Buelin b = new Buelin();
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10], 15, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10, 10], 15, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, -10, 10], 15, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 0], 15, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10], 15, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10], 15, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null, 10, null, false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null, null, [10, -10], false); });
-            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null, null, [10, 0], false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10],           15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10, 10],   15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, -10, 10],      15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 0],        15, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10],       -12, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge([10, 10, 10],       0, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null,               10, null, false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null,               null, [10, -10], false); });
+            Assert.Throws<Exception>(() => { b.getSobrecostPerEquipatge(null,               null, [10, 0], false); });
         }
-         [TestMethod]
+        [TestMethod]
         public void TestBuelinGetPreu()
         {
             Buelin b;
             b = new Buelin();
-            
+
 
             Assert.AreEqual(0m, b.getSobrecostPerEquipatge(null, null, null, false));
             Assert.AreEqual(0m, b.getSobrecostPerEquipatge([30, 30, 30], 20, null, false));
@@ -34,10 +34,10 @@ namespace TestProject
             Assert.AreEqual(60m, b.getSobrecostPerEquipatge([30, 31, 30], 20, null, false));
             Assert.AreEqual(60m, b.getSobrecostPerEquipatge([30, 30, 31], 20, null, false));
             Assert.AreEqual(20m, b.getSobrecostPerEquipatge([30, 30, 30], 21, null, false));
-            Assert.AreEqual(0m, b.getSobrecostPerEquipatge(null, 20, [20, 20], false));
-            Assert.AreEqual(20m, b.getSobrecostPerEquipatge(null, 20, [20, 21], false));
-            Assert.AreEqual(200m, b.getSobrecostPerEquipatge(null, 20, [20, 20, 20], false));
-            Assert.AreEqual(220m, b.getSobrecostPerEquipatge(null, 20, [20, 21, 20], false));
+            Assert.AreEqual(0m, b.getSobrecostPerEquipatge(null, null, [20, 20], false));
+            Assert.AreEqual(20m, b.getSobrecostPerEquipatge(null, null, [20, 21], false));
+            Assert.AreEqual(200m, b.getSobrecostPerEquipatge(null, null, [20, 20, 20], false));
+            Assert.AreEqual(220m, b.getSobrecostPerEquipatge(null, null, [20, 21, 20], false));
             Assert.AreEqual(280m, b.getSobrecostPerEquipatge([30, 31, 30], 20, [20, 21, 20], false));
             Assert.AreEqual(282m, b.getSobrecostPerEquipatge([30, 31, 30], 20, [20, 21, 20], true));
             Assert.AreEqual(0m, b.getSobrecostPerEquipatge(null, null, null, true));
